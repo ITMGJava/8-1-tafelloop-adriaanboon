b7a132e4eada2b936ca97f898778111c8d0c8f40
adriaanboon <71772080+adriaanboon@users.noreply.github.com>
2020-09-29 17:20:32 +0200

    Toon 2 keer tafel van een ingevoerd getal.

--- src/TafelLoop.java
+++ src/TafelLoop.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class TafelLoop {
+
+    public static void main(String[] args) {
+
+    //keyboard variabele bevat invoer van het toetsenbord.
+
+    var keyboard = new Scanner(System.in);
+
+    int Aantalmaleninvoer=1;
+
+    //Reken de tafel van ingevoerd getal uit met een loop.
+
+    while (Aantalmaleninvoer <= 2) {
+        System.out.println("Voer een getal in, dan wordt de tafel van dat getal uitgerekend!");
+
+        //vang het ingevoerde getal af.
+        int invoerGetal=keyboard.nextInt();
+        Aantalmaleninvoer++;
+        for (int teller = 1; teller <= 10; teller++) {
+            int tafel = teller * invoerGetal;
+
+            System.out.print(teller);
+            System.out.print(" x " + invoerGetal + " = ");
+            System.out.println(tafel);
+        }
+    }
+    keyboard.close();
+  }
 }
